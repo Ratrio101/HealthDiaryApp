1ccaafed5793e1fd8fd97b3bcce68759988b83cb
Ratrio101 <vl4d.ivanovivanov@yandex.ru>
2026-02-08 12:38:45 +0800

    Добавлены папки, логика создания БД и сущности

---
+++ HealthDiaryApp/Database/HealthDatabase.cs
@@ -0,0 +1,40 @@
+﻿using HealthDiaryApp.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HealthDiaryApp.Database
+{
+    public class HealthDatabase
+    {
+
+        readonly SQLiteAsyncConnection database;
+
+        public HealthDatabase(string dbPath)
+        {
+            database = new SQLiteAsyncConnection(dbPath);
+            database.CreateTableAsync<HealthEntry>().Wait();
+        }
+
+        public Task<List<HealthEntry>> GetEntriesAsync()
+        {
+            return database.Table<HealthEntry>().ToListAsync();
+        }
+
+        public Task<int> SaveEntryAsync(HealthEntry entry)
+        {
+            if (entry.Id != 0)
+                return database.UpdateAsync(entry);
+            else
+                return database.InsertAsync(entry);
+        }
+
+        public Task<int> DeleteEntryAsync(HealthEntry entry)
+        {
+            return database.DeleteAsync(entry);
+        }
+
+
+    }
+}
---
+++ HealthDiaryApp/Models/HealthEntry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using SQLite;
+
+namespace HealthDiaryApp.Models
+{
+    public class HealthEntry
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        // Активность
+        public int Steps { get; set; }
+
+        // Сон
+        public double SleepHours { get; set; }
+
+        // Питание
+        public int Calories { get; set; }
+
+        // Показатели здоровья
+        public int Pulse { get; set; }
+        public int Pressure { get; set; }
+        public double Weight { get; set; }
+    }
+}
